5e3a45364b1ee4436e97aefd05ec5a851f4e8222
ravine13 <kuriaderek@gmail.com>
2023-10-03 15:13:19 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-});
+  document.addEventListener("DOMContentLoaded", () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit',(e) => {
+      e.preventDefault()
+      buildToDo(e.target.value)
+      form.reset()
+    })
+    
+  });
+  
+  function buildToDo(todo){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${todo}`
+    p.appendChild(btn)
+    console.log(p);
+    document.querySelector('#task')
+};
+
+
